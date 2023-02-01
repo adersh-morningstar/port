4b60c656f91941c0983fb02321d7c40d00a8bfd7
monkey-d-dragon <66262012+adersh-morningstar@users.noreply.github.com>
2023-02-01 07:42:28 +0530

    Update malayalam.js

--- malayalam.js
+++ malayalam.js
@@ -1,237 +1,45 @@
-try{
-var latt = document.getElementById("cat");
-var rat = latt.innerHTML;
-function adder(A){
-   latt.innerHTML = latt.innerHTML＋A ;　
-   };
 
-
- document.getElementById("cat").innerhtml = rat + 'j';
-
-document.addEventListener('keydown', (event) => {
-   if(event.key == " "   ){
- adder("ങ്ക");
-       };
-    else if(event.key == "q"   ){
- adder("ഴ");
-       };
-     else if(event.key == "q"  &&  event.key == "Shift" ){
-        adder("ഒ");
-       };
-    else if(event.key == "w"   ){
- adder("ജ ");
-       };
-     else if(event.key == "w"  &&  event.key == "Shift" ){
-        adder("ഴ");
-       };
-    else if(event.key == "e"   ){
- adder("പ");
-       };
-     else if(event.key == "e"  &&  event.key == "Shift" ){
-        adder("ഫ");
-       };
-    else if(event.key == "r"   ){
- adder(" ന");
-       };
-     else if(event.key == "r"  &&  event.key == "Shift" ){
-        adder("ൻ");
-       };
-    else if(event.key == "t"   ){
- adder("എ");
-       };
-     else if(event.key == "t"  &&  event.key == "Shift" ){
-        adder("ഇ");
-       };
-    else if(event.key == "y"   ){
- adder("റ");
-       };
-     else if(event.key == "y"  &&  event.key == "Shift" ){
-        adder("്ര");
-       };
-    else if(event.key == "u"   ){
- adder("യ");
-       };
-     else if(event.key == "u"  &&  event.key == "Shift" ){
-        adder("യ്");
-       };
-    else if(event.key == "i"   ){
- adder("വ");
-       };
-     else if(event.key == "i"  &&  event.key == "Shift" ){
-        adder("്വ");
-       };
-    else if(event.key == "o"   ){
- adder("ൂ");
-       };
-     else if(event.key == "o"  &&  event.key == "Shift" ){
-        adder("ൗ");
-       };
-    else if(event.key == "p"   ){
- adder("ദ");
-       };
-     else if(event.key == "p"  &&  event.key == "Shift" ){
-        adder("ധ");
-       };
-    else if(event.key == "["   ){
- adder(",");
-       };
-     else if(event.key == "["  &&  event.key == "Shift" ){
-        adder("ൃ");
-       };
-    else if(event.key == "]"   ){
- adder("ക്ക");
-       };
-     else if(event.key == "]"  &&  event.key == "Shift" ){
-        adder("ട്ട");
-       };
-    else if(event.key == "a"   ){
- adder("മ");
-       };
-     else if(event.key == "a"  &&  event.key == "Shift" ){
-        adder("ഠ");
-       };
-    else if(event.key == "s"   ){
- adder("െ");
-       };
-     else if(event.key == "s"  &&  event.key == "Shift" ){
-        adder("േ");
-       };
-    else if(event.key == "d"   ){
- adder("ക");
-       };
-     else if(event.key == "d"  &&  event.key == "Shift" ){
-        adder("ഖ");
-       };
-    else if(event.key == "f"   ){
- adder("ത");
-       };
-     else if(event.key == "f"  &&  event.key == "Shift" ){
-        adder("ഥ");
-       };
-    else if(event.key == "g"   ){
- adder("ര");
-       };
-     else if(event.key == "g"  &&  event.key == "Shift" ){
-        adder("ർ");
-       };
-    else if(event.key == "h"   ){
- adder("ല");
-       };
-     else if(event.key == "h"  &&  event.key == "Shift" ){
-        adder("ൽ");
-       };
-    else if(event.key == "j"   ){
- adder("ി");
-       };
-     else if(event.key == "j"  &&  event.key == "Shift" ){
-        adder("ീ");
-       };
-    else if(event.key == "k"   ){
- adder("ു");
-       };
-     else if(event.key == "k"  &&  event.key == "Shift" ){
-        adder("ഉ");
-       };
-    else if(event.key == "l"   ){
- adder("ാ");
-       };
-     else if(event.key == "l"  &&  event.key == "Shift" ){
-        adder("അ");
-       };
-    else if(event.key == ";"   ){
- adder(". ");
-       };
-     else if(event.key == ";"  &&  event.key == "Shift" ){
-        adder(";");
-       };
-    else if(event.key == "'"   ){
- adder("ന്ന");
-       };
-     else if(event.key == "'"  &&  event.key == "Shift" ){
-        adder(":");
-       };
-    else if(event.key == "z"   ){
- adder("ഗ");
-       };
-     else if(event.key == "z"  &&  event.key == "Shift" ){
-        adder("ഘ");
-       };
-    else if(event.key == "x"   ){
- adder("ള");
-       };
-     else if(event.key == "x"  &&  event.key == "Shift" ){
-        adder("ൾ");
-       };
-    else if(event.key == "c"   ){
- adder("സ");
-       };
-     else if(event.key == "c"  &&  event.key == "Shift" ){
-        adder("ശ");
-       };
-    else if(event.key == "v"   ){
- adder("ച");
-       };
-     else if(event.key == "v"  &&  event.key == "Shift" ){
-        adder("ഛ");
-       };
-    else if(event.key == "b"   ){
- adder("ങ");
-       };
-     else if(event.key == "b"  &&  event.key == "Shift" ){
-        adder("ഞ");
-       };
-    else if(event.key == "n"   ){
- adder("ണ");
-       };
-     else if(event.key == "n"  &&  event.key == "Shift" ){
-        adder("ഞ");
-       };
-    else if(event.key == "m"   ){
- adder("ട");
-       };
-     else if(event.key == "m"  &&  event.key == "Shift" ){
-        adder("റ്റ");
-       };
-    else if(event.key == ","   ){
- adder("ഷ");
-       };
-     else if(event.key == ","  &&  event.key == "Shift" ){
-        adder("ഹ");
-       };
-    else if(event.key == "."   ){
- adder("ഭ");
-       };
-     else if(event.key == "."  &&  event.key == "Shift" ){
-        adder("ബ");
-       };
-    else if(event.key == "/"   ){
- adder("സ");
-       };
-     else if(event.key == "/"  &&  event.key == "Shift" ){
-        adder("ഢ");
-       };
-    });
-}
-catch(err){
-document.getElementById("cat").innerHTML = err.message;
-}
-\*
 try {
   const adder = (latt, A) => {
     latt.innerHTML = latt.innerHTML + A;
   };
 
   const map = new Map([
-    [' ', 'ങ്ക'],
-    ['q', 'ഴ'],
-    ['Q', 'ഒ'],
-    ['w', 'ജ '],
-    ['W', 'ഴ'],
-    ['e', 'പ'],
-    ['E', 'ഫ'],
-    ['r', ' ന'],
-    ['R', 'ൻ'],
-    //... add more key/value pairs
+        ["q", "ഴ " ] , [ "Q " ,"ഒ"  ] ,
+        ["w", "ജ  " ] , [ "W " ,"ഴ"  ] ,
+        ["e", "പ " ] , [ "E " ,"ഫ"  ] ,
+        ["r", " ന " ] , [ "R " ,"ൻ"  ] ,
+        ["t", "എ " ] , [ "T " ,"ഇ"  ] ,
+        ["y", "റ " ] , [ "Y " ,"്ര"  ] ,
+        ["u", "യ " ] , [ "U " ,"യ്"  ] ,
+        ["i", "വ " ] , [ "I " ,"്വ"  ] ,
+        ["o", "ൂ " ] , [ "O " ,"ൗ"  ] ,
+        ["p", "ദ " ] , [ "P " ,"ധ"  ] ,
+        ["[", ", " ] , [ "{ " ,"ൃ"  ] ,
+        ["]", "ക്ക " ] , [ "} " ,"ട്ട"  ] ,
+        ["a", "മ " ] , [ "| " ,"ഠ"  ] ,
+        ["s", "െ " ] , [ "A " ,"േ"  ] ,
+        ["d", "ക " ] , [ "S " ,"ഖ"  ] ,
+        ["f", "ത " ] , [ "D " ,"ഥ"  ] ,
+        ["g", "ര " ] , [ "F " ,"ർ"  ] ,
+        ["h", "ല " ] , [ "G " ,"ൽ"  ] ,
+        ["j", "ി " ] , [ "H " ,"ീ"  ] ,
+        ["k", "ു " ] , [ "J " ,"ഉ"  ] ,
+        ["l", "ാ " ] , [ "K " ,"അ"  ] ,
+        [";", ".  " ] , [ "L " ,";"  ] ,
+        ["'", "ന്ന " ] , [ ": " ,":"  ] ,
+        ["z", "ഗ " ] , [ "" " ,"ഘ"  ] ,
+        ["x", "ള " ] , [ "Z " ,"ൾ"  ] ,
+        ["c", "സ " ] , [ "X " ,"ശ"  ] ,
+        ["v", "ച " ] , [ "C " ,"ഛ"  ] ,
+        ["b", "ങ " ] , [ "V " ,"ഞ"  ] ,
+        ["n", "ണ " ] , [ "B " ,"ഞ"  ] ,
+        ["m", "ട " ] , [ "N " ,"റ്റ"  ] ,
+        [",", "ഷ " ] , [ "M " ,"ഹ"  ] ,
+        [".", "ഭ " ] , [ "< " ,"ബ"  ] ,
+        ["/", "സ " ] , [ "> " ,"ഢ"  ] ,
+
+
   ]);
 
   const latt = document.getElementById('cat');
@@ -244,4 +52,4 @@ try {
 } catch (error) {
   console.error(error);
 }
-*/
+
